27baf98a5c1c36b8df84380d4c19a0acde261550
anlo4139 <94018154+annalow@users.noreply.github.com>
2022-10-12 17:22:03 -0600

    fixed bug

--- test/main.js
+++ test/main.js
@@ -1,12 +1,19 @@
-const windowWidth = window.innerWidth;
-const windowHeight = innerHeight;
+
 const myWindow = document.querySelector('body');
 const obj = document.querySelector('div');
 window.addEventListener("scroll", ()=>{
+    const windowWidth = window.innerWidth;
+    const windowHeight = innerHeight;
+    const objWidth = obj.innerWidth;
+    console.log(objWidth);
     //console.log('scroll');
     const scrollPosition = window.pageYOffset;
     //console.log(scrollPosition);
-    const moveObj = windowWidth * (scrollPosition / windowHeight); //obj will move horizontally in proportion to vertical scroll
+    if(scrollPosition >= objWidth){
+        var moveObj = windowWidth * (scrollPosition / windowHeight) - objWidth; //obj will move horizontally in proportion to vertical scroll accounting for object width
+    }else{
+        var moveObj = windowWidth * (scrollPosition / windowHeight); // prevents object from moving off the left of the page
+    }
     //console.log(moveObj);
     obj.style.left = moveObj + 'px';
 })
